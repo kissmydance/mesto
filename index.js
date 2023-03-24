8c27aac1bf29c69547f2cea5ac762c6459457d48
Sudakov Vladimir <sudakovvladimir60@gmail.com>
2023-03-24 15:26:47 +0500

    исправление ошибок

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const popupOpen = document.querySelector(".popup");
 const popupCloseButton = popupOpen.querySelector(".popup__close");
 const popupInputName = popupOpen.querySelector(".popup__input_type_name");
 const popupInputJob = popupOpen.querySelector(".popup__input_type_job");
-const popupSubmitButton = popupOpen.querySelector(".popup__form-button");
 const popupForm = popupOpen.querySelector(".popup__form");
 const popupProfileName = document.querySelector(".profile__name");
 const popupProfileJob = document.querySelector(".profile__job");
@@ -11,13 +10,13 @@ const popupProfileJob = document.querySelector(".profile__job");
 
 
 editingButton.addEventListener('click', () => {
-    popupOpen.classList.add("popup__opened");
+    popupOpen.classList.add("popup__active");
     popupInputName.value = popupProfileName.textContent;
     popupInputJob.value =  popupProfileJob.textContent;
 });
 
 popupCloseButton.addEventListener('click', () => {
-    popupOpen.classList.remove("popup__opened");
+    popupOpen.classList.remove("popup__active");
 });
 
 
@@ -26,6 +25,6 @@ popupForm.addEventListener('submit', (event) => {
     event.preventDefault();
     popupProfileName.textContent = popupInputName.value;
     popupProfileJob.textContent = popupInputJob.value;
-    popupOpen.classList.remove("popup__opened");
+    popupOpen.classList.remove("popup__active");
 
 });
